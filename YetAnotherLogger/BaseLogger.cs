8b1ee5884566c54986682b443d191f3d743fcede
AdmiringWorm <kim.nordmo@gmail.com>
2012-09-20 02:30:52 +0200

    Started the work on YAL

---
+++ YetAnotherLogger/BaseLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace YAL
+{
+    public abstract class BaseLogger
+    {
+        public static string AppName;
+        public static string FileName;
+        public static string BaseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public static BaseLogger Create()
+        {
+            return Create("FileLogger");
+        }
+
+        public static BaseLogger Create(string loggerName)
+        {
+            if (string.Compare(loggerName, "FileLogger", StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return new FileLogger();
+            }
+            else if (string.Compare(loggerName, "XmlLogger", StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return new XmlLogger();
+            }
+
+            throw new ArgumentException("The logger " + loggerName + " was not found.");
+        }
+
+        public abstract void Log(string type, string text);
+
+        public abstract void Log(LoggInfo loggInfo);
+    }
+}
---
+++ YetAnotherLogger/LoggInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YAL
+{
+    public class LoggInfo
+    {
+        public string Text { get; set; }
+        public LoggType Type { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+
+    public enum LoggType
+    {
+        Info,
+        Warning,
+        Error,
+        General,
+    }
+}
