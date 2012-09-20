c01df8b1b6c496f73059d2cc23ba1b21ea5fb589
AdmiringWorm <kim.nordmo@gmail.com>
2012-09-20 12:56:36 +0200

    Added a new static property which holds the Logger. Added Annotations

--- YetAnotherLogger/BaseLogger.cs
+++ YetAnotherLogger/BaseLogger.cs
@@ -1,38 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
 
 namespace YAL
 {
     public abstract class BaseLogger
     {
+        /// <summary>
+        /// The name of the Application Folder to be used.
+        /// </summary>
         public static string AppName;
+        /// <summary>
+        /// The File name to log in.
+        /// </summary>
         public static string FileName;
+        /// <summary>
+        /// The directory to save the log file in, excluding <see cref="AppName"/>.
+        /// </summary>
         public static string BaseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        protected static BaseLogger _default;
 
+        /// <summary>
+        /// Creates a new <see cref="FileLogger"/>
+        /// </summary>
+        /// <returns>The created <see cref="FileLogger"/></returns>
         public static BaseLogger Create()
         {
             return Create("FileLogger");
         }
 
+        /// <summary>
+        /// The <see cref="BaseLogger"/>.
+        /// NOTE: It will create a new <see cref="FileLogger"/>
+        /// if no logger has been created previously.
+        /// </summary>
+        public static BaseLogger Default
+        {
+            get
+            {
+                if (_default == null)
+                    return Create();
+
+                return _default;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="BaseLogger"/>
+        /// </summary>
+        /// <param name="loggerName">
+        /// The name of the logger to create.
+        /// Valid values:
+        /// FileLogger and XmlLogger
+        /// </param>
+        /// <returns>The new <see cref="BaseLogger"/></returns>
         public static BaseLogger Create(string loggerName)
         {
             if (string.Compare(loggerName, "FileLogger", StringComparison.OrdinalIgnoreCase) == 0)
             {
-                return new FileLogger();
+                _default = new FileLogger();
             }
             else if (string.Compare(loggerName, "XmlLogger", StringComparison.OrdinalIgnoreCase) == 0)
             {
-                return new XmlLogger();
+                _default = new XmlLogger();
             }
+            return _default;
 
             throw new ArgumentException("The logger " + loggerName + " was not found.");
         }
 
-        public abstract void Log(string type, string text);
+        /// <summary>
+        /// Logs a new line of text
+        /// </summary>
+        /// <param name="type">The type of the error to log</param>
+        /// <param name="text">The error message to log.</param>
+        public abstract void Log(LoggType type, string text);
 
+        /// <summary>
+        /// Logs an error
+        /// </summary>
+        /// <param name="loggInfo">The <see cref="LoggInfo"/> to log from.</param>
         public abstract void Log(LoggInfo loggInfo);
     }
 }
